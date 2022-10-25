36be57d486ca467741e6fc6bb386ccf92c6dd7c4
Курмангали <kurmangali0606@gmail.com>
2022-10-25 23:09:58 +0600

    -g

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const PORT = 4000;
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chat-client-sigma.vercel.app"],
   },
 });
 
@@ -41,5 +41,5 @@ app.use("/", router);
 io.on("connection", sockets);
 
 httpServer.listen(PORT, () => {
-  console.log("Server is running at http://localhost:4000");
+  console.log("Server is running at https://chat-tan-nine.vercel.app");
 });
