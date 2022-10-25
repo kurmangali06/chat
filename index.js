5176f24d83375527d936d8cb533483cfabe12e9c
Курмангали <kurmangali0606@gmail.com>
2022-10-25 23:25:11 +0600

    B

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
 const app = express();
 const PORT = 4000;
 
-// app.use(cors());
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
